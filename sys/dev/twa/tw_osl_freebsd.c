d49a5e75510aebf875f24a11acfed9a1f72bb79f
Dirk Meyer <dirk.meyer+git@dinoex.sub.org>
2024-04-04 15:54:30 +0200

    adapt for FreeBSD-14 API

--- sys/dev/twa/tw_osl_freebsd.c
+++ sys/dev/twa/tw_osl_freebsd.c
@@ -67,7 +67,7 @@ static struct cdevsw twa_cdevsw = {
 	.d_name =	"twa",
 };
 
-static devclass_t	twa_devclass;
+
 
 /*
  * Function name:	twa_open
@@ -194,7 +194,7 @@ static driver_t	twa_pci_driver = {
 	sizeof(struct twa_softc)
 };
 
-DRIVER_MODULE(twa, pci, twa_pci_driver, twa_devclass, 0, 0);
+DRIVER_MODULE(twa, pci, twa_pci_driver, 0, 0);
 MODULE_DEPEND(twa, cam, 1, 1, 1);
 MODULE_DEPEND(twa, pci, 1, 1, 1);
 
